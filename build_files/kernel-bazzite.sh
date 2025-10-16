ccef922e5cc1842154ab314312f5860562fd4d80
Hec <hec@heccraft.com>
2025-10-16 05:22:06 -0400

    Try to fix the bazzite kernel :)

--- build_files/kernel-bazzite.sh
+++ build_files/kernel-bazzite.sh
@@ -4,26 +4,28 @@
 
 set -ouex pipefail
 
+mkdir /tmp/bazzite-kernel-build
 KERNEL=$(skopeo inspect --retry-times 3 docker://ghcr.io/ublue-os/akmods:bazzite-"$(rpm -E %fedora)" | jq -r '.Labels["ostree.linux"]')
 
 # Download Bazzite kernel
-skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods:bazzite-"$(rpm -E %fedora)"-${KERNEL} dir:/tmp/akmods
-AKMODS_TARGZ=$(jq -r '.layers[].digest' </tmp/akmods/manifest.json | cut -d : -f 2)
-tar -xvzf /tmp/akmods/"$AKMODS_TARGZ" -C /tmp/
-mv /tmp/rpms/* /tmp/akmods/
+skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods:bazzite-"$(rpm -E %fedora)"-${KERNEL} dir:/tmp/bazzite-kernel-build/akmods
+AKMODS_TARGZ=$(jq -r '.layers[].digest' </tmp/bazzite-kernel-build/akmods/manifest.json | cut -d : -f 2)
+tar -xvzf /tmp/bazzite-kernel-build/akmods/"$AKMODS_TARGZ" -C /tmp/bazzite-kernel-build
+mv /tmp/bazzite-kernel-build/rpms/* /tmp/bazzite-kernel-build/akmods/
 
 # Remove existing kernel WITHOUT removing anything else
 dnf5 versionlock delete kernel{-core,-modules,-modules-core,-modules-extra,-tools,-tools-libs,-headers,-devel}
-dnf5 -y remove --no-autoremove kernel{,-core,-modules,-modules-core,-modules-extra,-tools,-tools-libs,-headers}
+dnf5 -y remove --no-autoremove kernel-modules kernel-modules-core kernel-modules-extra
 
 # Install downloaded Bazzite kernel
-dnf5 -y --setopt=disable_excludes=* install /tmp/kernel-rpms/*.rpm
-dnf5 versionlock add kernel{,-core,-modules,-modules-core,-modules-extra,-tools,-tools-lib,-headers,-devel,-devel-matched}
+dnf5 -y install /tmp/bazzite-kernel-build/kernel-rpms/kernel{,-core,-modules,-modules-core,-modules-extra}-${KERNEL}.rpm
+dnf5 versionlock add kernel{,-core,-modules,-modules-core,-modules-extra,-tools,-tools-libs,-headers,-devel,-devel-matched}
 # reinstall virtualbox-guest-additions that got caught in the crossfire 
 dnf5 -y install virtualbox-guest-additions
 
 # Build initramfs
 KERNEL_SUFFIX=""
+export DRACUT_NO_XATTR=1 # i think this fixes something? i have no idea
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel(|'"$KERNEL_SUFFIX"'-)//')"
 /usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
